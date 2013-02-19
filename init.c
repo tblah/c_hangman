8c8b4cc5c942a8178107f572c0b088e1c69734ef
Tom Eccles <tomdow2@gmail.com>
2013-02-19 00:28:48 +0000

    The aforementioned bug has beeen fixed. Yippie!. The fix has been implamented by searching for the first charichter which is not a string and then copying the string from this point on to the begginning of the string.

--- init.c
+++ init.c
@@ -4,7 +4,22 @@
 
 #include <stdio.h>
 #include <stdlib.h> // for exit()
-#include <string.h> // for strchr()
+#include <string.h> // for strchr() and memmove() and strlen()
+#include <assert.h> // for assert()
+#include <ctype.h> // for isspace()
+
+void stripLeadingSpaces(char* s){
+	/* used to count through the array to find the first charichter that is not a space*/
+	char* pcPosCounter = s;
+	
+	assert(s); // make sure s is not NULL
+	/* keep counting through the array untll either NULL or a value which is not a space is encountered */	
+	while (*pcPosCounter != '\0' && isspace(*pcPosCounter)){
+		++pcPosCounter;
+	}
+	/* move the charichters from after the spaces to the begginning of the string */
+	memmove(s, pcPosCounter, strlen(pcPosCounter));	
+}
 
 void replaceChar(char* s, char c){
 	char* pcReplace = 0; // as a NULL pointer so that it cannot be used yet
@@ -21,6 +36,7 @@ void getWord(char* s, int length){
 		exit(-1);
 	}
 
+	stripLeadingSpaces(s); // remove all leading spaces from s (otherwise the string would start with a null after below
 	replaceChar(s, ' '); // make the string end at the first space
 	replaceChar(s, '\n'); // remove any newlines
 
